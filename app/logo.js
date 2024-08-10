65e16079a17267a0f9777840448f57b222c25cd9
KARL <krf0821@gmail.com>
2024-08-10 22:54:02 +0100

    Changed gpt version

---
+++ app/logo.js
@@ -0,0 +1,8 @@
+export const logtoConfig = {
+    endpoint: 'https://ur4vss.logto.app/',
+    appId: '0pq6raepbc6pl5zyoo2di',
+    appSecret: 'gCmAViSQDRMVGFo6HGROWBiVCor1wSVJ',
+    baseUrl: 'http://localhost:3000', // Change to your own base URL
+    cookieSecret: 'AOXUaXF8xax897YXZM1Vp16VRtOAvCp9', // Auto-generated 32 digit secret
+    cookieSecure: process.env.NODE_ENV === 'production',
+  };
